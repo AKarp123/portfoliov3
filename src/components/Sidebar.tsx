a97cde2fa9b863b47f1e0fbe21d4691909cad53c
AKarp123 <ashtonkarp63@gmail.com>
2025-11-26 18:40:42 -0500

    layoyt

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,23 @@
-const Sidebar = () => {
-	return (
-		<div className="flex max-h-screen min-h-screen max-w-[20%] min-w-[20%] flex-col items-end justify-center">
-			<a href="/">Home</a>
-			<br className=""/>
-			<a href="/projects">Projects</a>
+const Sidebar = ({ currentPath } : { currentPath: string}) => {
 
+	const pages = [
+		{ name: "Home", href: "/" },
+		{ name: "Projects", href: "/projects" },
+	]
+
+
+
+	return (
+		<div className="max-h-screen min-h-screen max-w-[20%] min-w-[20%] md:flex-col items-end justify-center hidden md:flex">
+			{pages.map((page) => (
+				<a
+					key={page.name}
+					href={page.href}
+					className={`${currentPath === page.href ? "font-bold" : "text-gray-700"} font-pixel text-[24px]`}
+				>
+					{page.name}
+				</a>
+			))}
 		</div>
 	);
 };
